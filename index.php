ce2b94aaaeb7685abf02ce6e9855aabace1bdca5
agilbaka <devi.krisdiansyah@gmail.com>
2017-02-01 21:46:09 +0700

    homepage

---
+++ index.php
@@ -0,0 +1,276 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="index,nofollow" />
+    <meta name="copyright" content="Copyright © 2017" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="google" content="notranslate" />
+    <title>rentnow</title>
+
+    <meta name="keywords"       content="#" />
+    <meta name="url"            content="#" />
+    <meta name="description"    content="#" />
+
+    <meta name="twitter:card" content="app">
+    <meta name="twitter:site" content="@#" />
+    <meta name="twitter:creator" content="@#" />
+    <meta name="twitter:url" content="#">
+    <meta name="twitter:title" content="#">
+    <meta name="twitter:description" content="#">
+    <meta name="twitter:image" content="##">
+
+
+    <meta property="og:url"                content="#" />
+    <meta property="og:type"               content="article" />
+    <meta property="og:title"              content="#" />
+    <meta property="og:description"        content="#" />
+    <meta name="author"         content="#">
+    <meta name="csrf-token"     content="#" />
+    <meta name="image:Logo"     content="#"/>
+    <meta property="og:image"   content="#"/>
+
+    <link rel="canonical" href="#" />
+
+    <link rel="apple-touch-icon" sizes="57x57" href="'images/fav/apple-icon-57x57.png'">
+    <link rel="apple-touch-icon" sizes="60x60" href="'images/fav/apple-icon-57x57.png'">
+    <link rel="apple-touch-icon" sizes="72x72" href="'images/fav/apple-icon-72x72.png'">
+    <link rel="apple-touch-icon" sizes="76x76" href="'images/fav/apple-icon-76x76.png'">
+    <link rel="apple-touch-icon" sizes="114x114" href="'images/fav/apple-icon-114x114.png'">
+    <link rel="apple-touch-icon" sizes="120x120" href="'images/fav/apple-icon-120x120.png'">
+    <link rel="apple-touch-icon" sizes="144x144" href="'images/fav/apple-icon-144x144.png'">
+    <link rel="apple-touch-icon" sizes="152x152" href="'images/fav/apple-icon-152x152.png'">
+    <link rel="apple-touch-icon" sizes="180x180" href="'images/fav/apple-icon-180x180.png'">
+    <link rel="icon" type="image/png" sizes="192x192"  href="'images/fav/android-icon-192x192.png'">
+    <link rel="icon" type="image/png" sizes="32x32" href="'images/fav/favicon-32x32.png'">
+    <link rel="icon" type="image/png" sizes="96x96" href="'images/fav/favicon-96x96.png'">
+    <link rel="icon" type="image/png" sizes="16x16" href="'images/fav/favicon-16x16.png'">
+    <link rel="manifest" href="/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="'images//ms-icon-144x144.png'">
+    <meta name="theme-color" content="#ffffff">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,700" rel="stylesheet">
+    <link rel="stylesheet" href="dist/css/app.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css">
+
+</head>
+<body>
+    <nav class="navigation">
+        <div class="container">
+            <a href="" class="logo"></a>
+            <div class="menu">
+                <ul>
+                    <li>
+                        <a href="">Lease Your</a>
+                        <ul class="submenu">
+                            <li><a href="">Transportation</a></li>
+                            <li><a href="">House</a></li>
+                            <li><a href="">Tools</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                <div class="box-login">
+                    <a href="" class="sign-up">Sign Up</a>
+                    <a href="">Login</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <header class="header-section">
+        <div class="header-content">
+            <h1>Justo Amet Lorem</h1>
+        </div>
+    </header>
+    <section class="section section-stories">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2 class="heading">Fusce pretium sagittis</h2>    
+                    <div class="list-stories">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <h3 class="sub-heading">Suspendisse eu justo</h3>
+                            </div>
+                            <div class="col-md-8">
+                                <figure class="cover-stories">
+                                    <a href="">
+                                        <img src="https://images.pexels.com/photos/175979/pexels-photo-175979.jpeg?w=940&h=650&auto=compress&cs=tinysrgb" alt="">
+                                    </a>
+                                </figure>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="box-stories">
+                                    <a href="" class="link-stories">Mauris nibh quam</a>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet inventore, rerum dicta quod dolor est fuga ratione provident maiores aperiam, non illo error autem nisi minus in, facilis. Incidunt, eum.</p>
+                                    <a href="">Read more</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="list-stories">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <h3 class="sub-heading">Suspendisse eu justo</h3>
+                            </div>
+                            <div class="col-md-8">
+                                <figure class="cover-stories">
+                                    <a href="">
+                                        <img src="https://images.pexels.com/photos/6553/person-holiday-vacation-woman.jpg?w=940&h=650&auto=compress&cs=tinysrgb" alt="">
+                                    </a>
+                                </figure>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="box-stories">
+                                    <a href="" class="link-stories">Mauris nibh quam</a>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet inventore, rerum dicta quod dolor est fuga ratione provident maiores aperiam, non illo error autem nisi minus in, facilis. Incidunt, eum.</p>
+                                    <a href="">Read more</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <a href="" class="btn-primary-o btn-big">See all stories</a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="section section-cities">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   <h2 class="heading">Mauris nec augue</h2> 
+                </div>
+                <div class="col-md-12">
+                    <div class="catalogue-cities">
+                        <div class="bg-dark big-city">
+                            <a href="" class="big-font">
+                             <img src="https://images.pexels.com/photos/50632/pexels-photo-50632.jpeg?w=940&h=650&auto=compress&cs=tinysrgb" alt="" class="img-cover">
+                             <label>
+                                london
+                                <span class="btn-white-o">explore</span>   
+                             </label>
+                            </a>
+                        </div>
+                        <div class="box-landspace-cities">
+                            <div class="bg-dark landscape-city">
+                                <a href="" class="big-font">
+                                    <img src="https://images.pexels.com/photos/5463/people-eiffel-tower-france-landmark.jpg?w=940&h=650&auto=compress&cs=tinysrgb" alt="" class="img-cover">
+                                    <label>
+                                        france
+                                        <span class="btn-white-o">explore</span>   
+                                    </label>
+                                </a>
+                            </div>
+                            <div class="bg-dark landscape-city">
+                                <a href="" class="big-font">
+                                    <img src="https://images.pexels.com/photos/2324/skyline-buildings-new-york-skyscrapers.jpg?w=940&h=650&auto=compress&cs=tinysrgb" alt="" class="img-cover">
+                                    <label>
+                                        US
+                                        <span class="btn-white-o">explore</span>
+                                    </label>   
+                                </a>
+                            </div>
+                        </div>
+                        <div class="clearfix"></div>
+                        <div class="bg-dark square-city">
+                            <a href="">
+                                <img src="https://images.pexels.com/photos/709/nature-mountain-volcano-asia.jpg?w=940&h=650&auto=compress&cs=tinysrgb" alt="" class="img-cover">
+                                <label>
+                                    Indonesia
+                                    <span class="btn-white-o">explore</span>
+                                </label>   
+                            </a>
+                        </div>
+                        <div class="bg-dark square-city">
+                            <a href="">
+                                <img src="https://cache-graphicslib.viator.com/graphicslib/thumbs674x446/2484/SITours/corcovado-mountain-and-christ-redeemer-statue-half-day-tour-in-rio-de-janeiro-128058.jpg" alt="" class="img-cover">
+                                <label>
+                                    Brazil
+                                    <span class="btn-white-o">explore</span>  
+                                </label> 
+                            </a>
+                        </div>
+                        <div class="bg-dark square-city">
+                            <a href="">
+                                <img src="https://images.pexels.com/photos/27954/pexels-photo-27954.jpg?h=350&auto=compress&cs=tinysrgb" alt="" class="img-cover">
+                                <label>
+                                    Italy
+                                    <span class="btn-white-o">explore</span>
+                                </label>   
+                            </a>
+                        </div>
+                        <div class="bg-dark more">
+                            <a href="">
+                                <img src="https://images.pexels.com/photos/27954/pexels-photo-27954.jpg?h=350&auto=compress&cs=tinysrgb" alt="" class="img-cover">
+                                <label class="btn-white-o">See more</label>   
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="box-social-media">
+                        <ul>
+                            <li>
+                                <a href=""><i class="zmdi zmdi-instagram"></i></a>
+                            </li>
+                            <li>
+                                <a href=""><i class="zmdi zmdi-twitter"></i></a>
+                            </li>
+                            <li>
+                                <a href=""><i class="zmdi zmdi-facebook"></i></a>
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-5">
+                    <ul class="link-footer">
+                        <li>
+                            <a href="">About Us</a>
+                        </li>
+                        <li>
+                            <a href="">Contact Us</a>
+                        </li>
+                        <li>
+                            <a href="">FAQ</a>
+                        </li>
+                        <li>
+                            <a href="">Terms of Use</a>
+                        </li>
+
+                    </ul>
+                </div>
+                <div class="col-md-4">
+                    <label for="subscribe"></label>
+                    <div class="box-subscribe">
+                        <form action="" class="default-form">
+                            <input type="text" placeholder="Email" name="subscribe">
+                            <input type="submit" value="subscribe" class="btn-primary">
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <script type="text/javascript"  src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function () {
+            var heightHeader = $('header.header-section').outerHeight() - 100;
+            $(window).scroll(function () {
+                var scolling  =  $(window).scrollTop();
+                if(scolling > heightHeader){
+                    $('nav.navigation').addClass('navigation-block');
+                }else {
+                    $('nav.navigation').removeClass('navigation-block');
+                }
+            })
+        })
+    </script>
+</body>
+</html>
