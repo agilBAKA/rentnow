3d49df07ee0d27f0b49775cb09314430da7e7511
agilbaka <devi.krisdiansyah@gmail.com>
2017-02-05 22:59:50 +0700

    profile

---
+++ modules/pages/profile/index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <?php require 'modules/components/head.php'; ?>
+
+</head>
+<body class="body-pages">
+	<div class="nav-pages">
+   		<?php require 'modules/components/nav.php'; ?>
+   	</div>
+    <section class="section section-profile">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                     <header class="header-profile">
+                         <figure class="big-avatar">
+                            <img src="http://socialpro.miguelvasquez.net/public/avatar/large_johndoe_18gu2qv.jpg" alt="" class="img-cover">
+                         </figure>
+                         <div class="header-content">
+                             <h1>Hey, I’m Taylor!</h1>
+                             <span>Jakarta, Indonesia · Member since July 2015</span>
+                             <p>
+                                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem nihil praesentium nisi eaque illum voluptate temporibus veniam commodi corrupti odio aut aliquam cupiditate sequi eius, eligendi labore laudantium provident animi.
+                             </p>
+                             <div class="wallet">
+                                 <label>Balance</label>
+                                 <button><i class="zmdi zmdi-plus-circle-o"></i></button>
+                                 <div class="box-balance">
+                                     Rp 200.000.000,-
+                                 </div>
+                             </div>
+                         </div>
+                     </header>
+                </div>
+                <div class="clearfix"></div>
+                
+                    <div class="col-md-4">
+                        <h2>Verified ID</h2>
+                        <div class="box-verifications">
+                            <ul>
+                                <li>
+                                    <label>Email address</label>
+                                    <span>Verified</span>
+                                    <i class="done"></i>
+                                </li>
+                                <li>
+                                    <label>Phone number</label>
+                                    <span>**********77</span>
+                                    <i class="done"></i>
+                                </li>
+                                <li>
+                                    <label>Facebook</label>
+                                    <span>1392 Friends</span>
+                                    <i class="done"></i>
+                                </li>
+                                <li>
+                                    <label>Offline ID</label>
+                                    <span>Passport</span>
+                                    <i class="done"></i>
+                                </li>
+                            </ul>
+                            <a href="">Add More Verifications</a>
+                        </div>
+                    </div>
+                    <div class="col-md-8">
+                        <div class="profile-wishlist">
+                            <h2>Wish List</h2>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="card-profile"></div>
+                                </div>
+                            </div>
+                            <div class="pagination">
+                                <ul>
+                                    <li><a href="" class="current">1</a></li>
+                                    <li><a href="">2</a></li>
+                                    <li><a href="">3</a></li>
+                                    <li><a href="">4</a></li>
+                                    <li><a href="">5</a></li>
+                                    <li><a href="" class="next btn-primary-o">Next Page</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+               
+             </div>
+        </div>
+    </section>
+    
+    <?php require 'modules/components/footer.php'; ?>
+    
+    
+</body>
+</html>
